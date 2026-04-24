cf29570960bf822c26944134193c801f5ef5394d
k25-b <k256572@nu.edu.pk>
2026-04-24 22:17:16 +0500

    Create Task4.c

---
+++ Task4.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+struct Patient {
+    char name[50];
+    int age;
+    char bloodType[5];
+    int patientID;
+    char diagnosis[100];
+};
+
+// display all patients
+void displayAll(struct Patient p[], int n) {
+    printf("\nAll Patient Records:\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("\nPatient %d:\n", i + 1);
+        printf("ID: %d\n", p[i].patientID);
+        printf("Name: %s", p[i].name);
+        printf("Age: %d\n", p[i].age);
+        printf("Blood Type: %s\n", p[i].bloodType);
+        printf("Diagnosis: %s", p[i].diagnosis);
+    }
+}
+
+// search by ID
+void searchByID(struct Patient p[], int n, int id) {
+    int found = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (p[i].patientID == id) {
+            printf("\nPatient Found:\n");
+            printf("ID: %d\n", p[i].patientID);
+            printf("Name: %s", p[i].name);
+            printf("Age: %d\n", p[i].age);
+            printf("Blood Type: %s\n", p[i].bloodType);
+            printf("Diagnosis: %s", p[i].diagnosis);
+            found = 1;
+        }
+    }
+
+    if (found == 0) {
+        printf("Patient not found\n");
+    }
+}
+
+int main() {
+    struct Patient p[5];
+
+    // input
+    for (int i = 0; i < 5; i++) {
+        printf("\nEnter details for patient %d\n", i + 1);
+
+        printf("Enter ID: ");
+        scanf("%d", &p[i].patientID);
+
+        printf("Enter Name: ");
+        getchar(); // clear buffer
+        fgets(p[i].name, 50, stdin);
+
+        printf("Enter Age: ");
+        scanf("%d", &p[i].age);
+
+        printf("Enter Blood Type: ");
+        scanf("%s", p[i].bloodType);
+
+        printf("Enter Diagnosis: ");
+        getchar(); // clear buffer again
+        fgets(p[i].diagnosis, 100, stdin);
+    }
+
+    // show all
+    displayAll(p, 5);
+
+    // search
+    int id;
+    printf("\nEnter ID to search: ");
+    scanf("%d", &id);
+
+    searchByID(p, 5, id);
+
+    return 0;
+}
